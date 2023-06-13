1178714e555e31c5fcaf3ffbe8cee93cad5fea15
Marian Turchyn <marian.turchyn@oril.co>
2023-06-13 11:24:34 +0300

    deploy

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amp1": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "AMP1": {
       "AppClientID": "string",
